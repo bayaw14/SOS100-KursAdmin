c2475030b00a43f36d58ed1559c47115116efde7
ataaalshbib <031591At@>
2026-03-16 13:04:43 +0100

    Update Program.cs with session, HttpClient and default route

--- SOS100-KursAdmin-MVC/Program.cs
+++ SOS100-KursAdmin-MVC/Program.cs
@@ -8,6 +8,14 @@ public class Program
 
         // Add services to the container.
         builder.Services.AddControllersWithViews();
+        builder.Services.AddHttpClient();
+        builder.Services.AddDistributedMemoryCache();
+        builder.Services.AddSession(options =>
+        {
+            options.IdleTimeout        = TimeSpan.FromHours(2);
+            options.Cookie.HttpOnly    = true;
+            options.Cookie.IsEssential = true;
+        });
 
         var app = builder.Build();
 
@@ -20,15 +28,17 @@ public class Program
         }
 
         app.UseHttpsRedirection();
+        app.UseStaticFiles();
         app.UseRouting();
+        app.UseSession();
 
         app.UseAuthorization();
 
-        app.MapStaticAssets();
+       
         app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}")
-            .WithStaticAssets();
+                pattern: "{controller=Inloggning}/{action=Index}/{id?}");
+            
 
         app.Run();
     }
